ecca2d3686bdcb734bb345e98633645ffcb7edce
Nikita Lebedev <nlebedev@reksoft.ru>
2012-09-25 22:12:09 +0400

    Dirty migration on draft-hixie-76

--- src/websocket_client.erl
+++ src/websocket_client.erl
@@ -13,7 +13,7 @@
 -behaviour(gen_server).
 
 %% API
--export([start/3,start/4,write/1,close/0]).
+-export([start/3,start/4,write/2,close/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -23,41 +23,43 @@
 -define(CONNECTING,0).
 -define(OPEN,1).
 -define(CLOSED,2).
+-define(HANDSHAKE,3).
 
 %% Behaviour definition
 -export([behaviour_info/1]).
 
 behaviour_info(callbacks) ->
-    [{ws_onmessage,1},{ws_onopen,0},{ws_onclose,0},{ws_close,0},{ws_send,1}];
+    [{ws_onmessage,1},{ws_onopen,1},{ws_onclose,0},{ws_close,0},{ws_send,1},
+     {ws_oninfo,1}];
 behaviour_info(_) ->
     undefined.
 
--record(state, {socket,readystate=undefined,headers=[],callback}).
+-record(state, {socket, readystate = undefined,
+                headers = [], callback, buffer = []}).
 
 start(Host,Port,Mod) ->
   start(Host,Port,"/",Mod).
   
 start(Host,Port,Path,Mod) ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [{Host,Port,Path,Mod}], []).
+    gen_server:start_link(?MODULE, [{Host,Port,Path,Mod}], []).
 
 init(Args) ->
-    process_flag(trap_exit,true),
+    %%process_flag(trap_exit,true),
     [{Host,Port,Path,Mod}] = Args,
-    {ok, Sock} = gen_tcp:connect(Host,Port,[binary,{packet, 0},{active,true}]),
+    {ok, Sock} = gen_tcp:connect(Host,Port,[binary,{packet, http},{active,true}]),
     
     Req = initial_request(Host,Path),
     ok = gen_tcp:send(Sock,Req),
-    inet:setopts(Sock, [{packet, http}]),
     
     {ok,#state{socket=Sock,callback=Mod}}.
 
 %% Write to the server
-write(Data) ->
-    gen_server:cast(?MODULE,{send,Data}).
+write(Pid, Data) ->
+    gen_server:cast(Pid,{send,Data}).
 
 %% Close the socket
-close() ->
-    gen_server:cast(?MODULE,close).
+close(Pid) ->
+    gen_server:cast(Pid,close).
 
 handle_cast({send,Data}, State) ->
     gen_tcp:send(State#state.socket,[0] ++ Data ++ [255]),
@@ -71,7 +73,7 @@ handle_cast(close,State) ->
     {stop,normal,State1}.
 
 %% Start handshake
-handle_info({http,Socket,{http_response,{1,1},101,"Web Socket Protocol Handshake"}}, State) ->
+handle_info({http,Socket,{http_response,{1,1},101,"WebSocket Protocol Handshake"}}, State) ->
     State1 = State#state{readystate=?CONNECTING,socket=Socket},
     {noreply, State1};
 
@@ -96,9 +98,9 @@ handle_info({http,Socket,http_eoh},State) ->
 	     case proplists:get_value('Upgrade',Headers) of
 		 "WebSocket" ->
 		     inet:setopts(Socket, [{packet, raw}]),
-		     State1 = State#state{readystate=?OPEN,socket=Socket},
+		     State1 = State#state{readystate=?HANDSHAKE,socket=Socket},
 		     Mod = State#state.callback,
-		     Mod:ws_onopen(),
+		     Mod:ws_onopen(self()),
 		     {noreply,State1};
 		 _Any  ->
 		     {stop,error,State}
@@ -109,13 +111,16 @@ handle_info({http,Socket,http_eoh},State) ->
     end;
 
 %% Handshake complete, handle packets
+handle_info({tcp, _Socket, <<_:128>>},#state{readystate = ?HANDSHAKE} = State) ->
+    {noreply, State#state{readystate = ?OPEN}};
+
 handle_info({tcp, _Socket, Data},State) ->
     case State#state.readystate of
 	?OPEN ->
-	    D = unframe(binary_to_list(Data)),
+	    {Msgs, BufferedState} = unframe(binary_to_list(Data), State),
 	    Mod = State#state.callback,
-	    Mod:ws_onmessage(D),
-	    {noreply,State};
+	    [ Mod:ws_onmessage(Msg) || Msg <- Msgs ],
+	    {noreply, BufferedState};
 	_Any ->
 	    {stop,error,State}
     end;
@@ -129,7 +134,11 @@ handle_info({tcp_error, _Socket, _Reason},State) ->
     {stop,tcp_error,State};
 
 handle_info({'EXIT', _Pid, _Reason},State) ->
-    {noreply,State}.
+    {noreply,State};
+
+handle_info(Something, #state{callback = Mod} = State) ->
+    Mod:ws_oninfo(Something),
+    {noreply, State}.
   
 handle_call(_Request,_From,State) ->
     {reply,ok,State}.
@@ -145,14 +154,29 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%--------------------------------------------------------------------
 initial_request(Host,Path) ->
-    "GET "++ Path ++" HTTP/1.1\r\nUpgrade: WebSocket\r\nConnection: Upgrade\r\n" ++ 
-	"Host: " ++ Host ++ "\r\n" ++
-	"Origin: http://" ++ Host ++ "/\r\n\r\n" ++
-        "draft-hixie: 68".
+    "GET "++ Path ++" HTTP/1.1\r\nUpgrade: websocket\r\nConnection: Upgrade\r\n" ++ 
+	"Host: " ++ Host ++ ":8081\r\n" ++
+	"Origin: http://" ++ Host ++ ":8081/\r\n" ++
+    "Sec-WebSocket-Key1: " ++ sec_key() ++ "\r\n" ++
+    "Sec-WebSocket-Key2: " ++ sec_key() ++ "\r\n" ++
+    "\r\n" ++ binary_to_list(<<45:64>>). %% HARDCODE
 
+sec_key() ->
+    "18x 6]8vM;54 *(5:  {   U1]8  z [  8". %% HARDCODE
 
-unframe([0|T]) -> unframe1(T).
-unframe1([255]) -> [];
-unframe1([H|T]) -> [H|unframe1(T)].
+unframe(Msg, #state{buffer = Buf} = State) ->
+    {Msgs, NewBuf} = unframe1(Msg, [], Buf),
+    {lists:reverse(Msgs), State#state{buffer = NewBuf}}.
 
-    
+unframe1([255 | Msg], Msgs, ByteAcc) ->
+    unframe2(Msg, [lists:reverse(ByteAcc) | Msgs], []);
+unframe1([Byte | Msg], Msgs, ByteAcc) ->
+    unframe1(Msg, Msgs, [Byte | ByteAcc]);
+unframe1([], Msgs, ByteAcc) ->
+    {Msgs, ByteAcc}.
+
+unframe2([0 | Msg], Msgs, ByteAcc) ->
+    unframe1(Msg, Msgs, ByteAcc);
+unframe2([], Msgs, ByteAcc) ->
+    {Msgs, ByteAcc}.
+
